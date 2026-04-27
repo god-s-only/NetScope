46fbf92c84d0cc320c7cf8bc2c1f0a63a529622f
god-s-only <joshxchris098@gmail.com>
2026-04-27 14:48:48 +0100

    bro

--- app/src/main/java/com/netscope/app/domain/model/DnsEntry.kt
+++ app/src/main/java/com/netscope/app/domain/model/DnsEntry.kt
@@ -1,3 +1,14 @@
 package com.netscope.app.domain.model
 
-data class DnsEntry()
+data class DnsEntry(
+    val id: String,
+    val timestampMs: Long,
+    val domain: String,
+    val resolvedIps: List<String>,
+    val queryType: DnsQueryType,
+    val uid: Int,
+    val appInfo: AppInfo? = null,
+    val responseTimeMs: Long? = null,
+)
+
+enum class DnsQueryType { A, AAAA, CNAME, MX, TXT, UNKNOWN }
