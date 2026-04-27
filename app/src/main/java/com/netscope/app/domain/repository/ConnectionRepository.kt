03d353d81ffcae83d68cda4e70378a4108375c7e
god-s-only <joshxchris098@gmail.com>
2026-04-27 15:15:17 +0100

    feat: connection repository

---
+++ app/src/main/java/com/netscope/app/domain/repository/ConnectionRepository.kt
@@ -0,0 +1,10 @@
+package com.netscope.app.domain.repository
+
+import com.netscope.app.domain.model.ConnectionEntry
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectionRepository {
+    fun observeConnections(): Flow<List<ConnectionEntry>>
+    fun observeActiveConnections(): Flow<List<ConnectionEntry>>
+    suspend fun clearAll()
+}
