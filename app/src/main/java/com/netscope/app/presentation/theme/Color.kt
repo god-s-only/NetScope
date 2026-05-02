7d9c861e2da2aeab80c61cbfa325b6a62b1a5ad4
god-s-only <joshxchris098@gmail.com>
2026-05-02 13:23:09 +0100

    feat: colors

--- app/src/main/java/com/netscope/app/presentation/theme/Color.kt
+++ app/src/main/java/com/netscope/app/presentation/theme/Color.kt
@@ -2,10 +2,26 @@ package com.netscope.app.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val NetScopePrimary    = Color(0xFF00BFA5)
+val NetScopeSecondary  = Color(0xFF1DE9B6)
+val NetScopeBackground = Color(0xFF0D1117)
+val NetScopeSurface    = Color(0xFF161B22)
+val NetScopeSurface2   = Color(0xFF21262D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NetScopeError      = Color(0xFFFF5370)
+val NetScopeSuccess    = Color(0xFF3FB950)
+val NetScopeWarning    = Color(0xFFD29922)
+val NetScopeInfo       = Color(0xFF58A6FF)
+
+val TextPrimary        = Color(0xFFE6EDF3)
+val TextSecondary      = Color(0xFF8B949E)
+val TextTertiary       = Color(0xFF484F58)
+
+val StatusSuccess      = Color(0xFF1A3A2A)
+val StatusSuccessText  = Color(0xFF3FB950)
+val StatusError        = Color(0xFF3A1A1A)
+val StatusErrorText    = Color(0xFFFF5370)
+val StatusWarning      = Color(0xFF3A2D0A)
+val StatusWarningText  = Color(0xFFD29922)
+val StatusPending      = Color(0xFF1A2A3A)
+val StatusPendingText  = Color(0xFF58A6FF)
