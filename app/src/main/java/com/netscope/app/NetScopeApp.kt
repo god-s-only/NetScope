4c0beedc2f7c986fc4874dbc356da0cbf0d1e0c4
god-s-only <joshxchris098@gmail.com>
2026-05-03 14:36:42 +0100

    feat Initialize the certificate manager on app start

--- app/src/main/java/com/netscope/app/NetScopeApp.kt
+++ app/src/main/java/com/netscope/app/NetScopeApp.kt
@@ -1,13 +1,24 @@
 package com.netscope.app
 
 import android.app.Application
+import com.netscope.app.data.proxy.cert.CertificateManager
 import dagger.hilt.android.HiltAndroidApp
+import org.conscrypt.BuildConfig
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NetScopeApp: Application() {
+class NetScopeApp : Application() {
+
+    @Inject
+    lateinit var certificateManager: CertificateManager
+
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        certificateManager.initialize()
+        Timber.d("NetScopeApp started")
     }
 }
