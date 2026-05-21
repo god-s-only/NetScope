a760c817b62f384be6c4c3e2f4ab13bb68f4e7bc
god-s-only <joshxchris098@gmail.com>
2026-05-21 21:43:43 +0100

    feat: Eagerly initialize DnsRepositoryImpl just like we did for TrafficRepositoryImpl

--- app/src/main/java/com/netscope/app/NetScopeApp.kt
+++ app/src/main/java/com/netscope/app/NetScopeApp.kt
@@ -3,6 +3,7 @@ package com.netscope.app
 import android.app.Application
 import android.util.Log
 import com.netscope.app.data.proxy.cert.CertificateManager
+import com.netscope.app.data.repository.DnsRepositoryImpl
 import com.netscope.app.data.repository.TrafficRepositoryImpl
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -10,11 +11,9 @@ import javax.inject.Inject
 @HiltAndroidApp
 class NetScopeApp : Application() {
 
-    @Inject
-    lateinit var certificateManager: CertificateManager
-
-    @Inject
-    lateinit var trafficRepository: TrafficRepositoryImpl
+    @Inject lateinit var certificateManager: CertificateManager
+    @Inject lateinit var trafficRepository: TrafficRepositoryImpl
+    @Inject lateinit var dnsRepository: DnsRepositoryImpl
 
     override fun onCreate() {
         super.onCreate()
