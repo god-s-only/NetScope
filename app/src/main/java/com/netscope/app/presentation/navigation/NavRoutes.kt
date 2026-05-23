d5b2af4ca1475e45e04ecbdc40112d95583bcf3b
god-s-only <joshxchris098@gmail.com>
2026-05-23 14:55:27 +0100

    feat: nav routes updated with connections route name

--- app/src/main/java/com/netscope/app/presentation/navigation/NavRoutes.kt
+++ app/src/main/java/com/netscope/app/presentation/navigation/NavRoutes.kt
@@ -7,6 +7,7 @@ object NavRoutes {
     const val TRAFFIC_DETAIL = "traffic_detail/{${NavArgs.TRANSACTION_ID}}"
     const val DNS = "dns"
     const val TIMELINE = "timeline"
+    const val CONNECTIONS = "connections"
     const val REPLAY = "replay/{${NavArgs.TRANSACTION_ID}}"
 
     fun trafficDetail(id: String) = "traffic_detail/$id"
